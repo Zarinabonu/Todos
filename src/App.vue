195565f915c9ca08dedbebf95ab2ebbd57499c37
Zarinabonu <zarinabonu199924@example.com>
2020-09-04 10:22:14 +0500

    style: margin and padding change in list

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,75 @@
 <template>
-  <section class="app">
-    <header>
-      <h1>todos</h1>
-      <div class="one-line">
+  <div>
+    <section class="app">
+      <header>
+        <h1>todos</h1>
+        <div class="one-line">
 
-        <button >
-          <img v-if="tasks.length === 0" @click="checkAll" src="../src/assets/tick.svg">
-          <img v-else-if="tasks.length === completedTasks.length" @click="UnCheckAll" src="../src/assets/delete-all.svg">
-          <img v-else @click="checkAll" src="../src/assets/tick.svg">
+          <button>
+            <img v-if="tasks.length === 0" @click="checkAll" src="../src/assets/tick.svg">
+            <img class="deleteButton" v-else-if="tasks.length === completedTasks.length" @click="UnCheckAll"
+                 src="../src/assets/delete-all.svg">
+            <img v-else @click="checkAll" src="../src/assets/tick.svg">
 
-        </button>
-        <input class="add-todo" v-model="task" @keypress.enter="addTodo" type="text"
-               placeholder="What needs to be done?"
-               autofocus>
-      </div>
+          </button>
+          <input class="add-todo" v-model="task" @keypress.enter="addTodo" type="text"
+                 placeholder="What needs to be done?"
+                 autofocus>
+        </div>
 
-    </header>
-    <section class="main">
-      <!--      <input class="check-all" @click="checkAll" type="checkbox">-->
-      <!--      <label for="check-all"></label>-->
-      <ul class="todo-list">
-        <li class="completed" v-for="task in getTasks" v-bind:key="task.id">
-          <div class="view">
-            <label>
-              <input class="toggle" v-model="task.completed" type="checkbox">
-              <span class="checkmark"></span>
-            </label>
+      </header>
+      <section class="main">
+        <!--      <input class="check-all" @click="checkAll" type="checkbox">-->
+        <!--      <label for="check-all"></label>-->
+        <ul class="todo-list">
+          <li class="completed" v-for="task in getTasks" v-bind:key="task.id">
+            <div class="view">
+              <label>
+                <input class="toggle" v-model="task.completed" type="checkbox">
+                <span class="checkmark"></span>
+              </label>
               <del class="todo-completed" v-if="task.completed == true">{{ task.task }}</del>
-            <label v-else="">{{ task.task }}</label>
-            <button @click="deleteTodo(task.id)">
-              <img src="../src/assets/cross.svg">
-            </button>
-          </div>
-        </li>
-      </ul>
-    </section>
-    <footer class="footer">
+              <label v-else="">{{ task.task }}</label>
+              <button @click="deleteTodo(task.id)">
+                <img src="../src/assets/cross.svg">
+              </button>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
       <span class="todo-count">
         <strong>{{ tasks.length }}</strong> items left
       </span>
-      <ul class="filters">
-        <li>
-          <a @click="filter = 'all'">All</a>
-          <!--          <button value="all" @click="filter = 'all'">All</button>-->
-        </li>
-        <li>
-          <a @click="filter = 'active'">Active</a>
-          <!--          <button value="all" @click="filter = 'all'">All</button>-->
-        </li>
-        <li>
-          <a @click="filter = 'completed'">Completed</a>
-          <!--          <button value="all" @click="filter = 'all'">All</button>-->
-        </li>
-      </ul>
-      <button @click="deleteCompleted(task.id)" v-if="completedTasks.length > 0" class="clear-completed">Clear Completed</button>
+        <ul class="filters">
+          <li>
+            <a @click="filter = 'all'">All</a>
+            <!--          <button value="all" @click="filter = 'all'">All</button>-->
+          </li>
+          <li>
+            <a @click="filter = 'active'">Active</a>
+            <!--          <button value="all" @click="filter = 'all'">All</button>-->
+          </li>
+          <li>
+            <a @click="filter = 'completed'">Completed</a>
+            <!--          <button value="all" @click="filter = 'all'">All</button>-->
+          </li>
+        </ul>
+        <button @click="deleteCompleted(task.id)" v-if="completedTasks.length > 0" class="clear-completed">Clear
+          Completed
+        </button>
+      </footer>
+    </section>
+    <footer class="info">
+      <p>Double click to edit a todo </p>
+      <p>Written by
+        <a href="#">Addy Osmani</a>
+      </p>
+      <p>Part of
+        <a href="#">TodoMVC</a>
+      </p>
     </footer>
-
-  </section>
-
+  </div>
 </template>
 
 
@@ -103,7 +115,7 @@ export default {
     UnCheckAll() {
       let allCompletedTasks = this.tasks.filter(task => task.completed)
       // let allCompletedTasks = this.tasks.filter(el => el.filter === 'completed')
-      console.log('1212'+ allCompletedTasks)
+      console.log('1212' + allCompletedTasks)
       for (var key in this.tasks) {
         if (this.tasks.length === allCompletedTasks.length) {
           this.tasks[key].completed = false
